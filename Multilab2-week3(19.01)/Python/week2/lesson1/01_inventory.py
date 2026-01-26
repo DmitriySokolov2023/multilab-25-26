04ab6c813d613949dd88d9d66a48162497c3fdc1
DmitriySokolov2023 <sokolovoff2023@gmail.com>
2026-01-26 22:15:39 +0300

    234

--- Multilab2-week3(19.01)/Python/week2/lesson1/01_inventory.py
+++ Multilab2-week3(19.01)/Python/week2/lesson1/01_inventory.py
@@ -17,10 +17,21 @@ while True:
         for i in range(len(inventory)):
           print(i+1," - ", inventory[i])
 
-    if choice == "2":
-      print("Здесь будет логика добавление предмета")
-    if choice == "3":
-      print("Здесь будет логика удаления предмета")
-    if choice == "0":
+    elif choice == "2":
+      item = input('Введите название предмета: ').strip()
+      if item == "":
+        print('Ввод пустой строки не допускается!')
+      else:
+        inventory.append(item)
+        print(f"Добавлено: {item}")
+
+    elif choice == "3":
+      index = int(input('Введи номер предмета.'))
+      if index < 0 or index > len(inventory):
+        print('Предмета под таким номером не существует!')
+      else: del inventory[index - 1]
+
+    elif choice == "0":
       print("Выход из программы.")
       break
+    else: print('Вы ввели неверную команду. Можно 1,2,3 или 0')
